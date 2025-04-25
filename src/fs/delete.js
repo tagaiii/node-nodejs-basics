a11b80d293ad2105779a190a521b4e88026ee10d
Kamalov Tagaibek <tagai.nambavan@gmail.com>
2025-04-25 19:44:38 +0600

    feat: implement delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, 'files', 'fileToRemove.txt');
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    await fs.access(filePath);
+    await fs.rm(filePath);
+  } catch (error) {
+    console.error('FS operation failed', error);
+  }
 };
 
 await remove();
