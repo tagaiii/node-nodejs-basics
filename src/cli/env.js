e3e65bac21c155ebda6a3de90905175d8550be53
Kamalov Tagaibek <tagai.nambavan@gmail.com>
2025-04-25 20:20:41 +0600

    feat: implement cli functions

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+  const envVars = Object.keys(process.env)
+    .filter((key) => key.startsWith('RSS'))
+    .reduce((acc, key) => {
+      acc[key] = process.env[key];
+      return acc;
+    }, {});
+
+  Object.keys(envVars).forEach((key) => console.log(`${key}=${envVars[key]}`));
 };
 
 parseEnv();
