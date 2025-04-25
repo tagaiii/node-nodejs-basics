c7a304fd069fc8b72ff020d187f66df9c78627a7
Kamalov Tagaibek <tagai.nambavan@gmail.com>
2025-04-25 18:08:28 +0600

    feat: implement create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
-const create = async () => {
-    // Write your code here 
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const filePath = path.join(__dirname, 'files', 'fresh.txt');
+const str = 'I am fresh and young';
+
+const create = async (filePath, str) => {
+  try {
+    await fs.writeFile(filePath, str, { flag: 'wx' });
+    console.log('File is created!');
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-await create();
+await create(filePath, str);
