57cead48c6876d635baadf5db1f55fa3d20eda0e
Kamalov Tagaibek <tagai.nambavan@gmail.com>
2025-04-26 14:51:14 +0600

    refactor: rewrite cjs module to esm module

---
+++ src/modules/esm.mjs
@@ -0,0 +1,48 @@
+import { sep } from 'path';
+import { release, version } from 'os';
+import { createServer as createServerHttp } from 'http';
+import './files/c.cjs';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+import fs from 'node:fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const random = Math.random();
+
+let unknownObject;
+if (random > 0.5) {
+  unknownObject = JSON.parse(
+    await fs.readFile(path.join(__dirname, 'files', 'a.json'))
+  );
+} else {
+  unknownObject = JSON.parse(
+    await fs.readFile(path.join(__dirname, 'files', 'b.json'))
+  );
+}
+
+console.log(`Release ${release()}`);
+console.log(`Version ${version()}`);
+console.log(`Path segment separator is "${sep}"`);
+
+console.log(`Path to current file is ${__filename}`);
+console.log(`Path to current directory is ${__dirname}`);
+
+const myServer = createServerHttp((_, res) => {
+  res.end('Request accepted');
+});
+
+const PORT = 3000;
+
+console.log(unknownObject);
+
+myServer.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+  console.log('To terminate it, use Ctrl+C combination');
+});
+
+export default {
+  unknownObject,
+  myServer,
+};
