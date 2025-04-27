b0f92328c44d8e56a1d64e3734f20d1fa629ee1c
Kamalov Tagaibek <tagai.nambavan@gmail.com>
2025-04-27 16:44:47 +0600

    feat: implement streams transform function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,22 @@
+import { stdin, stdout } from 'node:process';
+import { Transform } from 'node:stream';
+import { pipeline } from 'node:stream/promises';
+import { EOL } from 'node:os';
+
+console.log('Enter a string to reverse...');
+console.log('Type "exit" to quit');
+const reverse = new Transform({
+  transform(chunk, encoding, callback) {
+    if (chunk.toString().trim() === 'exit') {
+      process.exit();
+    }
+    this.push(chunk.toString().split('').reverse().join('') + EOL);
+    callback();
+  },
+});
+
 const transform = async () => {
-    // Write your code here 
+  await pipeline(stdin, reverse, stdout);
 };
 
 await transform();
