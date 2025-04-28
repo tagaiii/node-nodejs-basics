bc0ff6b91c0c54a9c6428942c82d3b2d4a824b7c
Kamalov Tagaibek <tagai.nambavan@gmail.com>
2025-04-28 18:06:51 +0600

    feat: implement worker threads functions

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,15 @@
-// n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+import { parentPort, workerData } from 'node:worker_threads';
+
+const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  const result = nthFibonacci(workerData);
+  try {
+    parentPort.postMessage({ status: 'delivered', data: result });
+  } catch {
+    parentPort.postMessage({ status: 'error', data: null });
+  }
 };
 
 sendResult();
